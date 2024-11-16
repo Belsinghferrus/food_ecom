7f6f0e4e3e844afda06ee99f2542d5303d171ecb
Belsinghferrus <ferrusbelsingh@gmail.com>
2024-11-16 17:13:04 +0530

    first commit

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Header.css'
+import Navbar from '../Navbar/Navbar'
+
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className='header-content'>
+        <h2>Order your favourite food</h2>
+        <p>choose from a diverse menu feauring a delicate way to express your emotion as the saying goes ny Good Food! Good Mood!</p>
+        <button>View Menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
